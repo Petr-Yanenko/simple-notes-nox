e7efa6482c8be6dfb63cae42624feac8105d4a99
Petr Yanenko <petr.yanenko@outlook.com>
2020-06-06 09:17:09 +0300

    Notes Model refactofing

--- simple_notes/Models/notes_model.h
+++ simple_notes/Models/notes_model.h
@@ -6,24 +6,36 @@
 //  Copyright (c) 2017 Petr Yanenko. All rights reserved.
 //
 
-#ifndef simple_notes_notes_model____FILEEXTENSION___
-#define simple_notes_notes_model____FILEEXTENSION___
+#ifndef sn_notes_model____FILEEXTENSION___
+#define sn_notes_model____FILEEXTENSION___
 
 #include "selected_list_model.h"
 #include "light_note.h"
 
-#define SIMPLE_NOTES_TYPE_NOTES_MODEL simple_notes_notes_model_get_type ()
-G_DECLARE_FINAL_TYPE(SimpleNotesNotesModel, simple_notes_notes_model, SIMPLE_NOTES, NOTES_MODEL, SimpleNotesSelectedListModel)
 
-SimpleNotesNotesModel *simple_notes_notes_model_new (guint64 folderID, SimpleNotesMediator *mediator);
+#define SN_TYPE_NOTES_MODEL sn_notes_model_get_type()
+G_DECLARE_FINAL_TYPE(SNNotesModel,
+		     sn_notes_model,
+		     SN,
+		     NOTES_MODEL,
+		     SNSelectedListModel)
 
-void simple_notes_notes_model_insert_note (SimpleNotesNotesModel *object, guint8 *content);
-SimpleNotesLightNote **simple_notes_notes_model_copy_notes (SimpleNotesNotesModel *object, guint *out_count);
-void simple_notes_notes_model_delete_note (SimpleNotesNotesModel *object, guint64 identifier);
-void simple_notes_notes_model_select_note (SimpleNotesNotesModel *object, guint64 identifier);
-void simple_notes_notes_model_change_note_content (SimpleNotesNotesModel *object, guint64 identifier, guint8 *newContent, guint len);
 
-guint64 simple_notes_notes_model_get_folder_id (SimpleNotesNotesModel *object);
-void simple_notes_notes_model_assign_folder_id (SimpleNotesNotesModel *object, guint64 folderID);
+SNNotesModel *
+sn_notes_model_new(void);
+
+
+void
+sn_notes_model_insert_note(SNNotesModel *object, gchar *content);
+
+SNLightNote **
+sn_notes_model_copy_notes(SNNotesModel *object, guint *out_count);
+
+void
+sn_notes_model_delete_note(SNNotesModel *object, guint64 identifier);
+
+void
+sn_notes_model_select_note(SNNotesModel *object, guint64 identifier);
+
 
 #endif
